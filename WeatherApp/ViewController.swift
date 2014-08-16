0e6ca6bd40eb43b434c30e37370631e17162b696
leon_huang <leon_huang@gemteks.com>
2014-08-16 14:30:57 +0800

    add sun icon to storyboard

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mCity: UILabel!
+    @IBOutlet weak var mIcon: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
         self.mCity.text = "Taipei"
+        self.mIcon.image = UIImage(named: "sun-icon.jpg")
     }
 
     override func didReceiveMemoryWarning() {
