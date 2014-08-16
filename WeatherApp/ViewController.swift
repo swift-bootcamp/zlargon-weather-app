28ca279f9034605d310b170778f9ebc2a3bcff36
leon_huang <leon_huang@gemteks.com>
2014-08-16 13:42:55 +0800

    init swift iOS porject with .gitignore
    https://github.com/github/gitignore/blob/master/Swift.gitignore

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by zlargon on 2014/8/16.
+//  Copyright (c) 2014年 zlargon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
